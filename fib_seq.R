8b2836943b6eb198aa5a7cb2d30a721019d77062
Adam Reimer <adam.reimer@alaska.gov>
2023-07-07 16:03:01 -0800

    remove maanual Fibonacci calculations
    manual calculations become redundent now that we are using recurstion to get the series.

--- fib_seq.R
+++ fib_seq.R
@@ -2,12 +2,7 @@
 #Adam Reimer & Carly Reimer
 
 fib_seq <- c(0, 1)
-fib_seq[3] <- fib_seq[1] + fib_seq[2]
-fib_seq[4] <- fib_seq[2] + fib_seq[3]
-fib_seq[5] <- fib_seq[3] + fib_seq[4]
-fib_seq[6] <- fib_seq[4] + fib_seq[5]
-fib_seq[7] <- fib_seq[5] + fib_seq[6]
-for (i in 8:51) fib_seq[i] <- sum(fib_seq[(i-1):(i-2)])
+for (i in 3:51) fib_seq[i] <- sum(fib_seq[(i-1):(i-2)])
   
 plot(1:50, fib_seq[2:51]/fib_seq[1:50], type = "l")
-{golden_ration <- fib_seq[51]/fib_seq[50]}
+{golden_ratio <- fib_seq[51]/fib_seq[50]}
