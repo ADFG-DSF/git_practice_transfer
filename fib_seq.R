a4c0f61249cfa240f7d2ec6d7ddb4ce148d851e8
Adam Reimer <adam.reimer@alaska.gov>
2023-07-10 10:15:16 -0800

    Added axis labels to golden ratio plot.
    Not a lot to say but hammering home the idea that more informative messages are better. In this case I added labels to demonstrate a merge conflict. Will add different labels to the same file/figure on branch develop to create the conflict.

--- fib_seq.R
+++ fib_seq.R
@@ -4,5 +4,8 @@
 fib_seq <- c(0, 1)
 for (i in 3:51) fib_seq[i] <- sum(fib_seq[(i-1):(i-2)])
   
-plot(1:50, fib_seq[2:51]/fib_seq[1:50], type = "l")
+plot(1:50, fib_seq[2:51]/fib_seq[1:50], 
+     type = "l", 
+     xlab = "Fibonacci Number in Denominator",
+     ylab = "Ratio Between Succesive Fibonacci Numbers")
 {golden_ratio <- fib_seq[51]/fib_seq[50]}
